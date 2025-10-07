0bc51c5fbb07c33f28ff3e12961b2dad2efde106
Ahmed Alaa <ahmedalaa575370@gmail.com>
2025-10-07 20:27:51 +0300

    initial project structure

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
 
-      },
     );
   }
 }
